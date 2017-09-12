3d1f49cab546dbd377a8fc20756be41643d585ca
Parul-Kudtarkar <parulbioinf@gmail.com>
2017-09-12 13:01:33 -0700

    programmatic access

---
+++ T2DREAM_metadata.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+import json
+
+
+EPILOG = '''
+%(prog)s GET the results of a search from an ENCODE server metadata in JSON or TSV format
+
+Basic Useage:
+
+    %(prog)s --infile file.txt
+    %(prog)s --infile TSTSR112545
+    
+    A single column file listing the  identifiers of the objects desired
+    Search accession
+
+    The output file format either JSON or TSV
+    This can be changed with '--format'
+
+'''
+
+def getArgs():
+    parser = argparse.ArgumentParser(
+        description=__doc__, epilog=EPILOG,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        )
+    parser.add_argument('--infile',
+                        help="File containing single column of accessions " +
+                        "or a single accession")
+    parser.add_argument('--outfile',
+                        help="Output file name", default='report.txt')
+    parser.add_argument('--format',
+                        help="Output format can be either JSON or TSV.  Default is JSON",
+                         default='JSON')
+    args = parser.parse_args()
+
+class get_metadata():
+        def __init__(self, args):
+            self.infile = args.infile
+            self.outfile = args.outfile
+            self.format = args.format
+
+def main():
+    args = getArgs()
+
+if __name__ == "__main__":
+    main()
