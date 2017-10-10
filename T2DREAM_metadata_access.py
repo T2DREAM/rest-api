c38f6e4fc0078e8ba7c0b75d082ffc669791d8f9
Parul-Kudtarkar <parulbioinf@gmail.com>
2017-10-10 14:31:45 -0700

    minor changes

--- T2DREAM_metadata_access.py
+++ T2DREAM_metadata_access.py
@@ -15,7 +15,13 @@ Basic Useage:
     
     A accession and metadata type - experiment, file, annotation
 '''
-
+def metadata_search(accession, type):
+    HEADERS = {'accept': 'application/json'}
+    URL = 'http://www.t2dream-demo.org/'
+    response = requests.get(URL + type  + '/' + accession, headers=HEADERS)
+    response_json_dict = response.json()
+    metadata_search_response = json.dumps(response_json_dict, indent=4, separators=(',', ': '))
+    return metadata_search_response
 def main():
     parser = argparse.ArgumentParser(
         description=__doc__, epilog=EPILOG,
@@ -26,10 +32,7 @@ def main():
     args = parser.parse_args()
     accession= args.accession
     type = args.type
-    HEADERS = {'accept': 'application/json'}
-    URL = 'http://www.t2dream-demo.org/'
-    response = requests.get(URL + type  + '/' + accession, headers=HEADERS)
-    response_json_dict = response.json()
-    print json.dumps(response_json_dict, indent=4, separators=(',', ': '))
+    metadata_result = metadata_search(accession,type)
+    print metadata_result
 if __name__ == "__main__":
     main()
